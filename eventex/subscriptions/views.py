04556d1bd1b4c48d8c13235fb77d23f44fad4690
Wallace Coelho <wac.almeida@gmail.com>
2016-02-12 15:47:56 -0200

    Cadastro e exibição dos speakers

--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -28,7 +28,8 @@ def create(request):
         return render(request, 'subscriptions/subscription_form.html',
                       {'form': form})
 
-    subscription = form.save()
+
+    subscription = Subscription.objects.create(**form.cleaned_data)
 
     # Send email
     _send_mail('Confirmação de Inscrição',
--- eventex/urls.py
+++ eventex/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
-from eventex.core.views import home
-from eventex.subscriptions.views import new, detail
+from eventex.core.views import home, speaker_detail
 
 urlpatterns = [
     url(r'^$', home, name='home'),
-    url(r'^admin/', admin.site.urls),
     url(r'^inscricao/', include('eventex.subscriptions.urls', namespace='subscriptions')),
+    url(r'^palestrantes/(?P<slug>[\w-]+)/$', speaker_detail, name='speaker_detail'),
+    url(r'^admin/', admin.site.urls),
 ]
